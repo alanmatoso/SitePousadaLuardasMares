b97db8b7fbaca4bb9205feb99d0044f090d131d1
Alan Cesario Matoso <alan.matoso@vectra>
2017-05-19 18:06:57 -0300

    Site da PousadaLuardasMares

---
+++ run-local/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Imobiliária Castro Lima</title>
+<style type="text/css">
+<!--
+.style1 {color: #0000FF}
+-->
+</style>
+</head>
+
+<body bgcolor=""">
+<p align="center"><img src="logomarca.jpg" width="1273" height="257" /><br clear="all" />
+</p>
+<p align="center"><iframe width="1280" height="720" src="https://www.youtube.com/embed/dxB3pEOoCPs" frameborder="0" allowfullscreen></iframe><br />
+  <div align="center"><div><strong>PABX:</strong> <strong>(81) 3465-1050</strong> / <strong>FAX: (81) 3466-0322</strong><br />
+  <strong>E-MAIL:</strong><span class="style1"> <a href="imobiliaria@castrolima.com.br">imobiliaria@castrolima.com.br</a> </span><br clear="all" />
+  Rua Santos Araujo, nº90 <br />
+Afogados - Recife - PE - CEP:   50770-270</p></div>
+<div align="right"><p align="right"><img src="construcao.jpg" width="128" height="107" /></p>
+<p align="right"><strong>P&aacute;gina em constru&ccedil;&atilde;o. </strong></p></div></div>
+<link rel="shortcut icon" href="logoc.ico" type="image/x-icon"/>
+</body>
+</html>
